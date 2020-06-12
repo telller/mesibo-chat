e0139000e3482405f2e39a624d147322a3a4548c
Vlad Golyachenko <vlad.golyachenko@zazmic.com>
2020-06-12 18:19:50 +0300

    upd token and group id

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,7 +1,7 @@
 export const BASE_URL = 'https://api.mesibo.com/api.php'
 
-export const MAIN_ACCESS_TOKEN = 'svw3ln0k023rd5pcoj96dpcv4f4p0rgopegil5sctykbd2x2xkd8p3w669j0203a'
+export const MAIN_ACCESS_TOKEN = 'fm7eajkniq14ieul4uzb752hpyn9dq6fwwkyf982zrtf7z297dvx7liwxhf42sz0'
 export const MAIN_APP_ID = '8128'
-export const GROUP_ID = '96346'
+export const GROUP_ID = '96408'
 
 export const DOWNLOAD_HISTORY_COUNT = 100
